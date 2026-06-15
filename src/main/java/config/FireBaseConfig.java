4e579e641c4216722e362ae31b9493637b1512c7
OscarErnesto19 <oscarneto35610@gmail.com>
2026-06-15 15:58:16 -0300

    APIV4

--- src/main/java/config/FireBaseConfig.java
+++ src/main/java/config/FireBaseConfig.java
@@ -1,14 +1,16 @@
 package config;
 
 import java.io.IOException;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
 import com.google.auth.oauth2.GoogleCredentials;
 import com.google.firebase.FirebaseApp;
 import com.google.firebase.FirebaseOptions;
 
 @Configuration
-public class FirebaseConfig {
+public class FireBaseConfig {
 
     @Bean
     FirebaseApp firebaseApp() throws IOException {
--- src/test/java/com/example/mobile/AppmobileApplicationTests.java
+++ src/test/java/com/example/mobile/AppmobileApplicationTests.java
@@ -1,4 +1,4 @@
-package com.example.demo;
+package com.example.mobile;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class AppmobileApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
